4e1007a651454779c520601c31a3e419ae0d5748
silvania <silvania.lopes.simoes@gmail.com>
2018-11-30 02:15:27 -0200

    ajustes

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
+function pegarUser(user) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(xhttp.responseText);
+        }
+    };
+
+    xhttp.open("GET", `https://api.github.com/users/${user}`, true);
+    xhttp.send();
+    
+}
+
 document.getElementById("button-ok").addEventListener("click", function () {
     event.preventDefault();
 
     var primeiroJogador = document.getElementById("input-jogador-1").value;
     var segundoJogador = document.getElementById("input-jogador-2").value;
 
-    function pegarUser(user) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var div = document.getElementById('demo');
-                div.innerHTML = xhttp.responseText;
-            }
-        };
-
-        xhttp.open("GET", `https://api.github.com/users/${user}`, true);
-        xhttp.send();
+    function chamar(){
+       pegarUser(primeiroJogador);
+       pegarUser(segundoJogador);
     }
-    pegarUser(primeiroJogador);
-    pegarUser(segundoJogador);
+    chamar();
 });
 
 
